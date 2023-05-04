6559e2b87bc46f07c323f7d8225224a31594f05d
Ramon Chaves <ramon.webap@gmail.com>
2023-05-04 00:35:22 -0300

    avançando no projeto

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Swith, Route} from 'react-route-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import Main from '.pages/Main';
-import Repositorio from './pages/repositorio';
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio';
 
-
-export default function Router(){
-    return(
-        <BrowserRouter>
-        <Swith>
-            <Router exact path="/" component={Main} />
-
-            <Router exact path="/repositorio:repositorio" component={Repositorio} />
-
-        </Swith>
-        </BrowserRouter>
-    );
+export default function Routes(){
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/repositorio/:repositorio" component={Repositorio} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
